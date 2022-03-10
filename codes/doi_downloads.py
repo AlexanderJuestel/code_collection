78fae9b88726e72e9facb433d6369a640ee2e928
AlexanderJuestel <45469915+AlexanderJuestel@users.noreply.github.com>
2022-03-10 14:50:49 +0100

    'AddDOIDownloadEarthDoc'

--- codes/doi_downloads.py
+++ codes/doi_downloads.py
@@ -5,8 +5,34 @@ Contributors: Alexander Jüstel
 
 import numpy as np
 import urllib3
-import os
 from typing import List
+
+
+def get_website_content(url: str):
+    """Function to retrieve data from a website
+
+    Parameters
+    __________
+
+        url: str
+            URL to the website
+
+    """
+
+    # Checking that the URL is of type string
+    if not isinstance(url, str):
+        raise TypeError('URL must be provided as string')
+
+    # Connect to URL
+    http_pool = urllib3.connection_from_url(url)
+
+    # Open URL
+    r = http_pool.urlopen('GET', url)
+
+    # Getting the data
+    website = r.data.decode('utf-8')
+
+    return website
 
 
 def download_dois_one_issue_solid_earth(volume: int,
@@ -35,14 +61,8 @@ def download_dois_one_issue_solid_earth(volume: int,
     # Defining the URL with volume number and issue number
     url = 'https://se.copernicus.org/articles/%s/issue%s.html' % (volume, issue)
 
-    # Connect to URL
-    http_pool = urllib3.connection_from_url(url)
-
-    # Open URL
-    r = http_pool.urlopen('GET', url)
-
-    # Getting the data
-    website = r.data.decode('utf-8')
+    # Retrieving website data
+    website = get_website_content(url)
 
     # Extracting the DOI numbers
     dois_se = ['https://doi.org/10.5194' + website.split('https://doi.org/10.5194')[i + 1].split(',')[0] for i in
@@ -144,6 +164,109 @@ def download_dois_solid_earth(volume_start: int = 1,
     return dois_se
 
 
+def download_dois_earthdoc(conference_url: str, titles_per_page: int = 20, page_number_start: int = 1, page_number_end: int = 100):
+    """ Function to download EarthDoc DOI Numbers
+
+    Parameters:
+    ___________
+
+        conference_url: str
+           URL of the Conference
+
+        titles_per_page: int
+            Number of titles that are displayed on one page
+
+        page_number_start: int
+            Page Number Start
+
+        page_number_end: int
+            Page Number End
+
+    """
+
+    # Checking that the conference URL is of type string
+    if not isinstance(conference_url, str):
+        raise TypeError('The conference or workshop URL must be provided as string')
+
+    # Checking that the titles per page value is of type int
+    if not isinstance(titles_per_page, int):
+        raise TypeError('The title of pages attribute must be of type int')
+
+    # Checking that the page number start is of type int
+    if not isinstance(page_number_start, int):
+        raise TypeError('The page number start must be provided as int')
+
+    # Checking that the page number end is of type int
+    if not isinstance(page_number_end, int):
+        raise TypeError('The page number end must be provided as int')
+
+    # Checking that the titles per page attribute is only either 20, 50 or 100
+    if titles_per_page not in [20, 50, 100]:
+        raise ValueError('Please use only either 20, 50 or 100 as a value for the titles per page')
+
+    # Creating array of pages
+    pages = np.arange(page_number_start, page_number_end+1,1)
+
+    # Creating empty list to store DOI numbers
+    dois_earthdoc = []
+
+    # Creating URLs and downloading DOIs
+    for page in pages:
+        url = create_earth_doc_url(conference_url=conference_url,
+                                   titles_per_page=titles_per_page,
+                                   page_number=page)
+
+        website = get_website_content(url)
+
+        doi_earthdoc = [website.split('content/papers/')[j+1].split('data-itemId')[0].split('dir=')[0].split('/-->')[0].split('>\n<i')[0].split('"')[0].split('>\n<i')[0].split(',http://')[0] for j in range(len(website.split('content/papers/'))-1)]
+
+        dois_earthdoc = dois_earthdoc + doi_earthdoc
+
+    return dois_earthdoc
+
+
+def create_earth_doc_url(conference_url: str, titles_per_page: int = 20, page_number: int = 1) -> str:
+    """ Function to create an URL of a webpage containing EarthDoc DOI Numbers
+
+    Parameters:
+    ___________
+
+        conference_url: str
+           URL of the Conference
+
+        titles_per_page: int
+            Number of titles that are displayed on one page
+
+        page_number: int
+            Page Number
+
+    """
+
+    # Checking that the conference URL is of type string
+    if not isinstance(conference_url, str):
+        raise TypeError('The conference or workshop URL must be provided as string')
+
+    # Checking that the titles per page value is of type int
+    if not isinstance(titles_per_page, int):
+        raise TypeError('The title of pages attribute must be of type int')
+
+    # Checking that the page number is of type int
+    if not isinstance(page_number, (int, np.int32)):
+        raise TypeError('The page number must be provided as int')
+
+    # Checking that the titles per page attribute is only either 20, 50 or 100
+    if titles_per_page not in [20, 50, 100]:
+        raise ValueError('Please use only either 20, 50 or 100 as a value for the titles per page')
+
+    # Creating URL for 20 titles per page
+    if titles_per_page == 20:
+        url = 'https://www.earthdoc.org/content/proceedings/' + conference_url + '?page=' + str(page_number)
+    else:
+        url = 'https://www.earthdoc.org/content/proceedings/' + conference_url + '?pageSize=' + str(titles_per_page) + '&page=' + str(page_number)
+
+    return url
+
+
 def save_doi_numbers(list_dois: list,
                      path: str):
     """Function to save list of DOIs to a text file
@@ -171,3 +294,5 @@ def save_doi_numbers(list_dois: list,
     with open(path, 'w') as f:
         for doi in list_dois:
             f.write("%s\n" % doi)
+
+    print('DOIs successfully saved to disc')
--- tests/test_doi_downloads.py
+++ tests/test_doi_downloads.py
@@ -5,6 +5,7 @@ Contributors: Alexander Jüstel
 
 import pytest
 
+
 def test_download_dois_one_issue_solid_earth():
     from codes.doi_downloads import download_dois_one_issue_solid_earth
 
@@ -32,6 +33,29 @@ def test_download_dois_solid_earth():
     assert all(isinstance(doi, str) for doi in dois_se)
 
 
+def test_get_website_content():
+    from codes.doi_downloads import get_website_content
+
+    website = get_website_content('https://se.copernicus.org/articles/%s/issue%s.html' % (1, 1))
+
+    assert isinstance(website, str)
 
 
+def test_create_earth_doc_url():
+    from codes.doi_downloads import create_earth_doc_url
 
+    url = create_earth_doc_url(conference_url='eageannual2021')
+
+    assert url == 'https://www.earthdoc.org/content/proceedings/eageannual2021?page=1'
+
+    url = create_earth_doc_url(conference_url='eageannual2021', page_number=2)
+
+    assert url == 'https://www.earthdoc.org/content/proceedings/eageannual2021?page=2'
+
+    url = create_earth_doc_url(conference_url='eageannual2021', titles_per_page=50)
+
+    assert url == 'https://www.earthdoc.org/content/proceedings/eageannual2021?pageSize=50&page=1'
+
+    url = create_earth_doc_url(conference_url='eageannual2021', titles_per_page=50, page_number=2)
+
+    assert url == 'https://www.earthdoc.org/content/proceedings/eageannual2021?pageSize=50&page=2'
